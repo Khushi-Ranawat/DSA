2535eb25627add7762be98953280b03e4a3750cb
Khushi Ranawat <ranawatkhushi449@gmail.com>
2026-07-18 00:46:53 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (56.83%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,16 +1,26 @@
 class Solution {
-    static int[] dp;
     public int rob(int[] arr) {
-        int n=arr.length;
-        dp=new int[n];
-        Arrays.fill(dp,-1);
-        return loot(0,arr);
-    }
-    private int loot(int i,int[] arr){
-        if(i>=arr.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int pick =arr[i] + loot(i+2,arr);
-        int skip=loot(i+1,arr);
-        return dp[i] =Math.max(pick,skip);
-    }
+        int n =arr.length;
+        int[] dp=new int[n];
+        dp[0] =arr[0];
+        if(n>1) dp[1]=Math.max(arr[0],arr[1]);
+        for(int i=2;i<n;i++){
+            dp[i] =Math.max(arr[i] + dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
+    }    
+    // static int[] dp;
+    // public int rob(int[] arr) {
+    //     int n=arr.length;
+    //     dp=new int[n];
+    //     Arrays.fill(dp,-1);
+    //     return loot(0,arr);
+    // }
+    // private int loot(int i,int[] arr){
+    //     if(i>=arr.length) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int pick =arr[i] + loot(i+2,arr);
+    //     int skip=loot(i+1,arr);
+    //     return dp[i] =Math.max(pick,skip);
+    // }
 }
